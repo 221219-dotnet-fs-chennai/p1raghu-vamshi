e4b59e843e04cbe68468567c194bc826ff28b5ae
Raghu-846 <raghuvamshi0114@gmail.com>
2022-12-21 10:48:29 +0530

    assignments are added

---
+++ assignments/Activity1.md
@@ -0,0 +1,7 @@
+mkdir Activities1
+cd Activities1
+vi Activity1.sh
+sh Activity1.sh
+echo 
+read
+
---
+++ assignments/Activity1.sh
@@ -0,0 +1,7 @@
+echo "welcome user"
+read -p "enter first name :" aname
+read -p "enter last name :" bname
+read -p "enter dob :" dob
+echo -e "\n $aname $bname"
+echo -e "\n $dob "
+i

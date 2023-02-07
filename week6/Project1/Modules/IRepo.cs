e1df7a2cd54a7995002f5238d0587558c5db85d7
Raghu-846 <raghuvamshi0114@gmail.com>
2023-02-07 09:22:23 +0530

    added Project1

---
+++ week6/Project1/Modules/IRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules
+{
+    public interface IRepo<T>
+    {
+        T Add(T t);
+        List<T> GetAll();
+        T RemoveUserDetails(int UserId);
+        T UpdateUserDetails(T t);
+
+
+    }
+}
---
+++ week6/Project1/service/Controllers/UserControler.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLogic;
+using Modules;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Caching.Memory;
+//using Microsoft.Data.SqlClient;
+//using Microsoft.Extensions.Caching.Memory;
+using Ninject.Activation.Caching;
+using System.Linq.Expressions;
+
+namespace service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserControler : ControllerBase
+    {
+        ILogic logic;
+        //  IMemoryCache cache;
+        public UserControler(ILogic logic) => this.logic = logic;
+      /*  {
+            logic = logic;
+
+        }*/
+        [HttpGet("FetchUser")]
+
+        public IActionResult Get()
+        {
+            try
+            {
+                var UserDetails = logic.GetUserDetails();
+                return Ok(UserDetails);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPost("AddTrainer")]
+        public IActionResult Add([FromBody] Modules.UserDetails userDetails)
+        {
+            try
+            {
+                var newuser = logic.AddUserDetails(userDetails);
+                return Created("Add", newuser);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("Delete")]
+        public IActionResult Delete([FromHeader] int UserId)
+        {
+            try
+            {
+                var userDel = logic.RemoveUserDetailsByUserId(UserId);
+                if (userDel != null)
+                {
+                    return Ok(userDel);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+    }
+}
+
